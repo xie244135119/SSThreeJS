31430530e2c7f1be7e0c91bf09ead41b1f1befc8
Murphy.xie <murphy.xie@yunzhihui.com>
2023-06-26 10:35:40 +0800

    adjust SSPostProcess

--- core/SSPostProcess.js
+++ core/SSPostProcess.js
@@ -1,4 +1,12 @@
+/*
+ * Author  Murphy.xie
+ * Date  2023-06-23 17:48:56
+ * LastEditors  Kayson.Wan
+ * LastEditTime  2023-06-24 20:12:18
+ * Description
+ */
 import * as THREE from 'three';
+// 后处理
 import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { FullScreenQuad, Pass } from 'three/examples/jsm/postprocessing/Pass';
@@ -23,10 +31,13 @@ import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader';
 import { BleachBypassShader } from 'three/examples/jsm/shaders/BleachBypassShader';
 import { SSRPass } from 'three/examples/jsm/postprocessing/SSRPass';
 import { ReflectorForSSRPass } from 'three/examples/jsm/objects/ReflectorForSSRPass';
+import GUI from 'lil-gui';
 import ThreeLoop from './SSThreeLoop';
 
-export default class SSPostProcess {
-  //
+export default class PostProcess {
+  /**
+   * @type SSThreeJs
+   */
   threeJs = null;
 
   /**
@@ -43,6 +54,24 @@ export default class SSPostProcess {
    * @type RenderPass
    */
   #renderPass = null;
+
+  /**
+   * 伽马矫正
+   * @param {ShaderPass}
+   */
+  #gammaPass = null;
+
+  /**
+   * bloom pass
+   * @param {UnrealBloomPass}
+   */
+  #unrealBloomPass = null;
+
+  /**
+   * debug gui
+   * @param {GUI} aThreeJs
+   */
+  #gui = null;
 
   constructor(aThreeJs) {
     this.threeJs = aThreeJs;
@@ -100,6 +129,12 @@ export default class SSPostProcess {
     if (effectComposer.passes.findIndex((item) => item === this.#renderPass) === -1) {
       effectComposer.addPass(this.#renderPass);
     }
+    if (this.#gammaPass === null) {
+      this.#gammaPass = new ShaderPass(GammaCorrectionShader);
+    }
+    if (effectComposer.passes.findIndex((item) => item === this.#gammaPass) === -1) {
+      effectComposer.addPass(this.#gammaPass);
+    }
     if (this.#outlinePass === null) {
       this.#outlinePass = new OutlinePass(
         { x: canvas.clientWidth, y: canvas.clientHeight },
@@ -134,15 +169,43 @@ export default class SSPostProcess {
     effectComposer.removePass(this.#outlinePass);
     this.disposeMaterialByPass(this.#renderPass);
     effectComposer.removePass(this.#renderPass);
+    this.disposeMaterialByPass(this.#gammaPass);
+    effectComposer.removePass(this.#gammaPass);
+  };
+
+  /**
+   * add bloom
+   */
+  addBloom = () => {
+    const effectComposer = this.getEffectComposer();
+    if (this.#unrealBloomPass === null) {
+      this.#unrealBloomPass = new UnrealBloomPass(
+        new THREE.Vector2(
+          this.threeJs.threeContainer.offsetWidth,
+          this.threeJs.threeContainer.offsetHeight
+        )
+      );
+    }
+    if (effectComposer.passes.findIndex((item) => item === this.#unrealBloomPass) === -1) {
+      this.effectComposer.addPass(this.#unrealBloomPass);
+    }
+  };
+
+  /**
+   * remove bloom
+   */
+  removeBloom = () => {
+    this.disposeMaterialByPass(this.#unrealBloomPass);
+    this.effectComposer.removePass(this.#unrealBloomPass);
   };
 
   /**
    * 增加覆盖盒
-   * @param {Array<THREE.Object3D>} aModels 一组模型体
+   * @param {Array<THREE.Object3D>} aObject3Ds 一组模型体
    * @param {THREE.MeshStandardMaterialParameters} materialParams 材质配置
    * @returns
    */
-  addMaskBoxByObject3Ds = (aObject3Ds = [], materialParams = '#00ffff') => {
+  addMaskBoxByObject3Ds = (aObject3Ds, materialParams) => {
     const material = new THREE.MeshStandardMaterial({
       name: 'tempmask',
       transparent: true,
@@ -156,14 +219,16 @@ export default class SSPostProcess {
     });
 
     aObject3Ds.forEach((item) => {
-      if (item instanceof THREE.Object3D) {
+      if (item.isObject3D) {
         if (item.userData?.tempMask) {
           item.userData.tempMask.visible = true;
+          item.userData.tempMask.position.copy(item.position);
           return;
         }
         const cloneitem = item.clone();
+        cloneitem.name = `${item.name}_tempmask`;
         cloneitem.traverse((obj) => {
-          if (obj instanceof THREE.Mesh) {
+          if (obj.type === 'Mesh') {
             obj.material = material;
           }
         });
@@ -179,9 +244,12 @@ export default class SSPostProcess {
    */
   removeMaskBox = (aObject3Ds = []) => {
     aObject3Ds.forEach((item) => {
-      if (item instanceof THREE.Object3D) {
-        if (item.userData?.tempMask instanceof THREE.Object3D) {
-          item.userData.tempMask.visible = false;
+      if (item.isObject3D) {
+        if (item.userData?.tempMask?.isObject3D) {
+          // item.userData.tempMask.visible = false;
+          this.threeJs.threeDisposeQueue.dispose(item.userData?.tempMask);
+          item.userData.tempMask.removeFromParent();
+          item.userData.tempMask = null;
         }
       }
     });
@@ -200,8 +268,8 @@ export default class SSPostProcess {
     const canvas = threeContainer;
     effectComposer.setSize(canvas.clientWidth, canvas.clientHeight);
     this.effectComposer = effectComposer;
-    // this.effectComposer.renderTarget1.texture.colorSpace = THREE.SRGBColorSpace;
-    // this.effectComposer.renderTarget2.texture.colorSpace = THREE.SRGBColorSpace;
+    // this.effectComposer.renderTarget1.texture.encoding = THREE.sRGBEncoding;
+    // this.effectComposer.renderTarget2.texture.encoding = THREE.sRGBEncoding;
     return effectComposer;
   };
 
@@ -240,7 +308,9 @@ export default class SSPostProcess {
    */
   openRender = () => {
     ThreeLoop.add(() => {
-      this.effectComposer.render();
+      if (this.effectComposer.passes.length > 0) {
+        this.effectComposer.render();
+      }
     }, 'postProcess update');
   };
 
@@ -250,4 +320,19 @@ export default class SSPostProcess {
   closeRender = () => {
     ThreeLoop.removeId('postProcess update');
   };
+
+  /**
+   * gui debug
+   */
+  #addGui = () => {
+    // const gui = new GUI({
+    //   closed: true,
+    // });
+    // this.#gui = gui;
+    // gui.show();
+    // const bloomfolder = gui.addFolder('辉光');
+    // bloomfolder.add({});
+  };
+
+  #addGuiFolder = () => {};
 }
